41614c9a698d7e6ab8fa1175de2900b34060220b
Dan Fowler <dan@logickc.com>
2016-02-21 15:30:05 -0600

    adding gitignore, actually

---
+++ haskell/ChapterThree/FirstThings.hs
@@ -0,0 +1,10 @@
+-- FirstThings.hs
+
+-- The symbol '::' is a type signature and  means "has the type"
+-- as in:
+-- "Hello!" :: [Char]
+-- which means:
+-- "Hello!" has the type [Char] (Character)
+
+-- 
+
