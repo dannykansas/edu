2a6992c0ac74f0bf74db639d414c2274047b772f
Dan Fowler <dan@logickc.com>
2016-02-21 00:28:26 -0600

    function tests

---
+++ haskell/ChapterTwo/FunctionWithLamda.hs
@@ -0,0 +1,7 @@
+printInc2 n = let plusTwo = n + 2
+              in print plusTwo
+
+-- turns into 
+
+printInc2' n =
+  (\plusTwo -> print plusTwo) (n + 2)
---
+++ haskell/ChapterTwo/FunctionWithLet.hs
@@ -0,0 +1,5 @@
+-- FunctionWithLet.hs
+module FuncionWithLet where
+
+printInc2 n = let plusTwo = n + 2
+              in print plusTwo
