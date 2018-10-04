90f7b4d69e89cad4b4f9ff8e2a3f34862a3e4ab2
Danny Fowler <danny.fowler@adroll.com>
2018-10-04 12:33:57 -0700

    trying a new comment format in shell scripting

--- dotfiles/osx/create_git_aliases.sh
+++ dotfiles/osx/create_git_aliases.sh
@@ -1,32 +1,34 @@
 #!/bin/sh
 
-# ==========================================
-# Overview: 
-#   A few nice shortcuts for git
-#
-# Why?:
-#   git has a built-in global config function!
-#   This will keep your dotfiles succinct.
-#  
-# Usage: 
-#   Just run once after installing git. 
-#   Rerun when new aliases are added.
-#
-# Table of Contents:
-#   1. git branches
-#   2. git cob  
-# ==========================================
+: '
+ Overview: 
+   A few nice shortcuts for git
 
-# 1. git branches 
-#
-# Usage: git branches
-# Output: branch commits sorted by asc chrono order
+ Why?:
+   git has a built-in global config function!
+   This will keep your dotfiles succinct.
+  
+ Usage: 
+   Just run once after installing git. 
+   Rerun when new aliases are added.
 
+ Table of Contents:
+   1. git branches
+   2. git cob  
+'
+
+: '
+  1. git branches 
+
+  Usage: git branches
+  Output: branch commits sorted by asc chrono order
+'
 git config --global alias.branches "!echo ' ------------------------------------------------------------' && git for-each-ref --sort='-authordate:iso8601' --format=' %(authordate:relative)%09%(refname:short)' refs/heads && echo ' ------------------------------------------------------------'"
 
-# 2. git cob (checkout new branch)
-# 
-# Usage: git cob <branchname>
-# Output: creates and switches to new branch
-
+: '
+  2. git cob (checkout new branch)
+ 
+  Usage: git cob <branchname>
+  Output: creates and switches to new branch
+'
 git config --global alias.cob "checkout -b"
