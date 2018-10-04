69c69f3923c832ac8ff19e80cb0541c460f8c2b6
Danny Fowler <danny.fowler@adroll.com>
2018-10-04 12:42:56 -0700

    remove comment style - bad visibility

--- dotfiles/osx/create_git_aliases.sh
+++ dotfiles/osx/create_git_aliases.sh
@@ -2,33 +2,35 @@
 
 : '
  Overview: 
+
    A few nice shortcuts for git
 
  Why?:
+
    git has a built-in global config function!
    This will keep your dotfiles succinct.
   
- Usage: 
+ Usage:
+ 
    Just run once after installing git. 
    Rerun when new aliases are added.
+      (yes, it is idempotent)
 
- Index of Alias Commands:
+ Index of Aliases:
    1. git branches
    2. git cob  
 '
 
-: '
-  1. git branches 
+#  1. git branches 
+#
+#  Usage: git branches
+#  Output: branch commits sorted by asc chrono order
 
-  Usage: git branches
-  Output: branch commits sorted by asc chrono order
-'
 git config --global alias.branches "!echo ' ------------------------------------------------------------' && git for-each-ref --sort='-authordate:iso8601' --format=' %(authordate:relative)%09%(refname:short)' refs/heads && echo ' ------------------------------------------------------------'"
 
-: '
-  2. git cob (checkout new branch)
- 
-  Usage: git cob <branchname>
-  Output: creates and switches to new branch
-'
+#  2. git cob (checkout new branch)
+#
+#  Usage: git cob <branchname>
+#  Output: creates and switches to new branch
+
 git config --global alias.cob "checkout -b"
