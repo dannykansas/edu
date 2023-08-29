a5bcb56faa2702764d17912eda6e2310c13dc67c
Danny Fowler <danny.fowler@nextroll.com>
2023-08-28 17:11:36 -0700

    broken create_links; stashing for now

--- dotfiles/zsh/create_links.sh
+++ dotfiles/zsh/create_links.sh
@@ -4,7 +4,12 @@ echo "Creating symlinks for dotfiles..."
 dotfiles=('.zshrc' '.zprofile' '.zsh.d/' '.zsh.d/aliases.zsh')
 
 for each in ${dotfiles[*]}; do
-  echo ${HOME}/$each
+  if [ -L ${HOME}/$each ]
+  then
+    echo "Skipping ${HOME}/$each as it already exists"
+  else 
+    echo ${HOME}/$each
+  fi
 done
 
 VRC=".vimrc"
